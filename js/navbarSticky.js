5be6baf290159cdf2c5a0fee436a4f2c43da74e3
Shalnark <65479699+Shounaks@users.noreply.github.com>
2021-04-27 19:38:59 +0530

    pushed version 1.0
    First Commit

---
+++ js/navbarSticky.js
@@ -0,0 +1,11 @@
+// window.onscroll = function() {stickyNavbar();};
+// var navbar = document.getElementById("customNavbar");
+// var sticky = navbar.offsetTop;
+
+// function stickyNavbar() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");  
+//   }
+// }
