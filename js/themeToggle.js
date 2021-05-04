766d1c7da4c797f74e7bf7a54d2e58fb7c0abdfe
Shalnark <65479699+Shounaks@users.noreply.github.com>
2021-05-04 14:40:31 +0530

    Fixed navbar ThemeChange

--- js/themeToggle.js
+++ js/themeToggle.js
@@ -1,9 +1,12 @@
 const btn = document.querySelector(".btn-toggle");
 const theme = document.querySelector("#theme-link");
+const themeNavbar = document.querySelector("#theme-link-navbar");
 const toggle = () => {
   if (theme.getAttribute("href") == "./css/custom-css/lightTheme.css") {
     theme.href = "./css/custom-css/darkTheme.css";
+    themeNavbar.href = "./css/custom-css/navbarDark.css";
   } else {
     theme.href = "./css/custom-css/lightTheme.css";
+    themeNavbar.href = "./css/custom-css/navbarLight.css";
   }
 };
