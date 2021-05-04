70f3a9ca3b1740a54f3310332e0965c18b61af59
Shalnark <65479699+Shounaks@users.noreply.github.com>
2021-05-05 00:50:01 +0530

    JoJo_ref2

---
+++ js/queen.js
@@ -0,0 +1,8 @@
+const showQueen = () => { 
+    var queen = document.getElementById("queen");
+    queen.style.display = "block";
+}
+const hideQueen = () => { 
+    var queen = document.getElementById("queen");
+    queen.style.display = "none";
+}
